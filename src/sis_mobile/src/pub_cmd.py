16d2a6187f54443ca4c2dc95f0a3717f90c0c91c
Andrea Censi <acensi@idsc.mavt.ethz.ch>
2018-11-22 16:12:45 +0100

    fix

--- src/sis_mobile/src/pub_cmd.py
+++ src/sis_mobile/src/pub_cmd.py
@@ -7,14 +7,14 @@ import rospy
 from geometry_msgs.msg import Twist
 
 if __name__ == '__main__':
-	if len(sys.argv)!= 3:
+	if len(sys.argv)<= 3:
 		print "Not enough arguments given, please give velocity and angular velocity!"
 		sys.exit(0)
         rospy.init_node("pub_cmd_node")
         pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
         twist = Twist()
         twist.linear.x = float(sys.argv[1])
-        twist.angular.z = float(sus.argv[2])
+        twist.angular.z = float(sys.argv[2])
         r = rospy.Rate(100) # Publish with 100Hz
         while not rospy.is_shutdown():
                 pub.publish(twist)
