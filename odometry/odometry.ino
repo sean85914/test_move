0235031b780c99002f1bb3cfa818ce1e987808f2
sean85914 <sean19960914@gmail.com>
2018-11-23 03:04:01 +0800

    revise some comment

--- odometry/odometry.ino
+++ odometry/odometry.ino
@@ -1,24 +1,26 @@
+/* Using encoder to calculate the angular velocity of the motors
+ * and transport them through serial to PI for control and dead  
+ * reckoning, for usage of differential-drive mobile robot.
+ * Developed by Sean Lu at Nov., 2018. 
+ */
 #define SPD_INT_L2 8
 #define SPD_INT_R2 9
-#define WIDTH 0.179
-#define RADIUS 0.032
-#define CPR 2970.0
+#define RADIUS 0.032 // Wheel radius, in meter
+#define CPR 2970.0   // Encoder Counts Per Revolution
 
 volatile long encoder_pre_L, encoder_pre_R; // present
 volatile long encoder_pos_L, encoder_pos_R; // post
 long time_;
-double theta;
 double hz = 50; // 50Hz
 
 void setup()
 {
   pinMode(SPD_INT_L2, INPUT);
   pinMode(SPD_INT_R2, INPUT);
-  theta = 0;
   encoder_pre_L = 0; encoder_pre_R = 0;
   encoder_pos_L = 0; encoder_pos_R = 0;
   Serial.begin(57600);
-  // regist interrupt callback function
+  // Regist interrupt callback functions
   // UNO pin 2, call Encoder_L when the signal is rising
   attachInterrupt(0, Encoder_L, RISING);
   // UNO pin 3, call Encoder_R when the signal is rising
@@ -29,15 +31,16 @@ void setup()
 void loop()
 {
   if(millis() - time_ >= 1000/hz) {
-    long dt = (millis() - time_); // ms
-    time_ = millis();
+    long dt = (millis() - time_); // Time difference, in ms
+    time_ = millis(); // Update time
+    // Calculate linear velocity
     double v_l = (encoder_pre_L - encoder_pos_L)*2*PI/CPR * RADIUS / dt * 1000;
     double v_r = (encoder_pre_R - encoder_pos_R)*2*PI/CPR * RADIUS / dt * 1000;
-    // update encoder
+    // Update encoder
     encoder_pos_L = encoder_pre_L;
     encoder_pos_R = encoder_pre_R;
-    // print data in serial
-    // data format:
+    // Print data in serial
+    // Data format:
     // v_r v_l
     Serial.print(v_r); Serial.print(" ");
     Serial.println(v_l);
