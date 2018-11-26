bf181f65aa7232989e502159b97d427c0ee0f2d7
sean85914 <sean19960914@gmail.com>
2018-11-26 16:27:52 +0800

    finish TBD

--- odometry_sw/odometry_sw.ino
+++ odometry_sw/odometry_sw.ino
@@ -11,15 +11,15 @@
  *  4-> RR: rear right
  */
  
-#define SPD_INT_FL #TBD
-#define SPD_INT_FR #TBD
-#define SPD_INT_RL #TBD
-#define SPD_INT_RR #TBD
+#define SPD_INT_FL 4
+#define SPD_INT_FR 5
+#define SPD_INT_RL 6
+#define SPD_INT_RR 7
 
-#define CPR #TBD   // Encoder Counts Per Revolution
+#define CPR 2970.0   // Encoder Counts Per Revolution
 
 volatile long encoder_pre_FL, encoder_pre_FR, encoder_pre_RL, encoder_pre_RR; // present
-volatile long encoder_posFL, encoder_pos_FR, encoder_pos_RL, encoder_pos_RR; // post
+volatile long encoder_pos_FL, encoder_pos_FR, encoder_pos_RL, encoder_pos_RR; // post
 long time_;
 double hz = 50; // 50Hz
 
@@ -34,13 +34,13 @@ void setup()
   Serial.begin(57600);
   // Regist interrupt callback functions
   // UNO pin 18, call Encoder_FL when the signal is rising
-  attachInterrupt(5, Encoder_L, RISING);
+  attachInterrupt(5, Encoder_FL, RISING);
   // UNO pin 19, call Encoder_FR when the signal is rising
   attachInterrupt(4, Encoder_FR, RISING);
-  // UNO pin 20, call Encoder_RR when the signal is rising
-  attachInterrupt(3, Encoder_RR, RISING);
-  // UNO pin 21, call Encoder_RL when the signal is rising
-  attachInterrupt(2, Encoder_RL, RISING);
+  // UNO pin 20, call Encoder_RL when the signal is rising
+  attachInterrupt(3, Encoder_RL, RISING);
+  // UNO pin 21, call Encoder_RR when the signal is rising
+  attachInterrupt(2, Encoder_RR, RISING);
   time_ = millis();
 }
 
@@ -49,11 +49,11 @@ void loop()
   if(millis() - time_ >= 1000/hz) {
     long dt = (millis() - time_); // Time difference, in ms
     time_ = millis(); // Update time
-    // Calculate linear velocity
-    double w_fl = (encoder_pre_FL - encoder_pos_FL)*2*PI/CPR / dt * 1000;
-    double w_fr = (encoder_pre_FR - encoder_pos_FR)*2*PI/CPR / dt * 1000;
-    double w_rl = (encoder_pre_RL - encoder_pos_RL)*2*PI/CPR / dt * 1000;
-    double w_rr = (encoder_pre_RR - encoder_pos_RR)*2*PI/CPR / dt * 1000;
+    // Calculate angular velocity, times 100 (100*rad/s)
+    double w_fl = (encoder_pre_FL - encoder_pos_FL)*2*PI/CPR / dt * 1000 * 100;
+    double w_fr = (encoder_pre_FR - encoder_pos_FR)*2*PI/CPR / dt * 1000 * 100;
+    double w_rl = (encoder_pre_RL - encoder_pos_RL)*2*PI/CPR / dt * 1000 * 100;
+    double w_rr = (encoder_pre_RR - encoder_pos_RR)*2*PI/CPR / dt * 1000 * 100;
     // Update encoder
     encoder_pos_FL = encoder_pre_FL;
     encoder_pos_FR = encoder_pre_FR;
@@ -67,12 +67,13 @@ void loop()
     Serial.print(w_rl); Serial.print(" ");
     Serial.println(w_rr);
   }
+  
 }
-# Direction TBD
+
 void Encoder_FL()
 {
-  if(digitalRead(SPD_INT_FL) == HIGH) --encoder_pre_FL;
-  else ++encoder_pre_FL;
+  if(digitalRead(SPD_INT_FL) == HIGH) ++encoder_pre_FL;
+  else --encoder_pre_FL;
 }
 
 void Encoder_FR()
@@ -83,8 +84,8 @@ void Encoder_FR()
 
 void Encoder_RL()
 {
-  if(digitalRead(SPD_INT_RL) == HIGH) --encoder_pre_RL;
-  else ++encoder_pre_RL;
+  if(digitalRead(SPD_INT_RL) == HIGH) ++encoder_pre_RL;
+  else --encoder_pre_RL;
 }
 
 void Encoder_RR()
