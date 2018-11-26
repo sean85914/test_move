0a514f5e50f80d099c10fee9f4b8608302fef028
sean85914 <sean19960914@gmail.com>
2018-11-26 19:22:11 +0800

    revise comment, change order

--- src/sis_mobile/src/pid_controller.py
+++ src/sis_mobile/src/pid_controller.py
@@ -13,11 +13,14 @@ WIDTH = 0.179
 RADIUS = 0.032
 
 # Check if the list is in range (low, up)
-# Param:
+# Params:
 # 	value_list:  given value list
-#   up:          upper bound
-#   low:         lower bound
-def in_range(value_list, up, low):
+#	low:         lower bound
+# 	up:          upper bound
+# Output:
+#   True: if every value in list in range (low, up)
+#   False: otherwise
+def in_range(value_list, low, up):
 	for i in range(0, len(value_list)):
 		if value_list[i] > up or value_list[i] < low:
 			return False
@@ -59,7 +62,7 @@ class Car_controller(object):
 			return # incorrect data
 		if len(data_list) != 2:
 			return # incorrect array size
-		if not in_range(data_list, 0.12, -0.12):
+		if not in_range(data_list, -0.12, 0.12):
 			return # data not in range
 		self.v_r, self.v_l = data_list
 		# dead reckoning
